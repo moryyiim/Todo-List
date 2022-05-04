7f36b6c728b97b3d9e353f5c5d6b7a7ee7ec03cf
Morelen Yim <morelenyim@gmail.com>
2022-05-03 18:08:47 -0700

    first commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
+  // Add Todos
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+  };
+
+  // Update Todos
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  // Remove Todos
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removeArr);
+  };
+
+  // Complete Todos
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>What's the Plan for Today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
+};
+
+export default TodoList;
